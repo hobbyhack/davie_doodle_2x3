35b6b6336e79595203d3195e3a919dc2b131ab4a
Shane Gary <522109+hobbyhack@users.noreply.github.com>
2024-10-07 11:54:36 -0400

    added doodle_2x3

---
+++ keyboards/doodle/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x65, 0x9A, 0x12, 0x53, 0x13, 0xE3, 0x39, 0x6E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 2 }
